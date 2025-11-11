a17ecd8482cbe1327bfc538897b7fae3af27d95c
Aditya Sharma <adityasharma626367@gmail.com>
2025-11-11 13:33:46 +0530

    feat: initialize frontend project with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development, build, linting, and preview.
    - Included Vite logo SVG in public directory.
    - Created main application component (App.jsx) with basic structure.
    - Added React logo SVG in assets directory.
    - Set up global CSS file (index.css) to import Tailwind CSS.
    - Implemented main entry point (main.jsx) to render the application.
    - Configured Vite with React and Tailwind CSS plugins in vite.config.js.

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,33 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+
+export const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    throw new ApiError(400, "All fields are required")
+  }
+
+  if (password.length < 6) {
+    throw new ApiError(400, "Password must be at least 6 characters long")
+  }
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    throw new ApiError(400, "User already exists")
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password
+  })
+
+  res.status(201).json(
+    new ApiResponse(201, "User created successfully", user)
+  )
+})
---
+++ backend/server.js
@@ -0,0 +1,45 @@
+import express from "express";
+import cors from "cors"
+import "dotenv/config"
+import { connectDB } from "./config/db.js";
+import { ApiResponse } from "./utils/ApiResponse.js";
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors(
+  {
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
+  }
+))
+
+//middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+// user routes
+import userRoutes from "./routes/user.routes.js"
+
+app.use("/api/v1/users", userRoutes)
+
+
+
+// error handler
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500
+  const message = err.message || "Internal server error";
+
+  return res.status(statusCode).json(
+    new ApiResponse(statusCode, message, {})
+  )
+
+})
+
+
+app.listen(PORT, () => {
+  connectDB()
+  console.log(`Server is running on port ${PORT}`);
+});
